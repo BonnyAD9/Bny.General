8e6965b49da2783a4cc8d1de506a716f51e0689a
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-12-03 23:22:50 +0100

    Add ConstPtr extensions

---
+++ Bny.General.Tester/ConstPtrExtensionsTester.cs
@@ -0,0 +1,53 @@
+﻿using Bny.General.Memory;
+
+namespace Bny.General.Tester;
+
+[UnitTest]
+internal class ConstPtrExtensionsTester
+{
+    [UnitTest]
+    public static void Test_IndexOf1(Asserter a)
+    {
+        int value = Random.Shared.Next(ushort.MaxValue);
+        ConstPtr<int> ptr = TestData.Generate(100, i => i + value);
+        int index = Random.Shared.Next(100);
+
+        a.Assert(ptr.IndexOf(index + value) == index);
+        a.Assert(ptr.IndexOf(index + 100 + value) == -1);
+    }
+
+    [UnitTest]
+    public static void Test_StartsWith(Asserter a)
+    {
+        int value = Random.Shared.Next(ushort.MaxValue);
+        ConstPtr<int> ptr = TestData.Generate(100, i => i + value);
+
+        a.Assert(ptr.StartsWith(ptr[..5]));
+        a.Assert(!ptr[..5].StartsWith(ptr));
+        a.Assert(!ptr.StartsWith(ptr[1..]));
+    }
+
+    [UnitTest]
+    public static void Test_IndexOfMultiple(Asserter a)
+    {
+        int value = Random.Shared.Next(ushort.MaxValue);
+        ConstPtr<int> ptr = TestData.Generate(100, i => i + value);
+        int index = Random.Shared.Next(90);
+        ConstPtr<int> other = TestData.Generate(10, i => i + index + value);
+
+        a.Assert(ptr.IndexOf(other) == index);
+        a.Assert(ptr.IndexOf(new int[] { 3, 2, 1 }) == -1);
+    }
+
+    [UnitTest]
+    public static void Test_IndexOfRepeat(Asserter a)
+    {
+        int value = Random.Shared.Next(ushort.MaxValue);
+        ConstPtr<int> ptr = TestData.Generate(100, i => i + value);
+        ConstPtr<int> other1 = new int[] { value + 6, value + 7 };
+        ConstPtr<int> other2 = new int[] { value + 7, value + 8 };
+
+        a.Assert(ptr.IndexOfRepeat(other1) == 6);
+        a.Assert(ptr.IndexOfRepeat(other2) == -1);
+    }
+}
---
+++ Bny.General/Memory/ConstPtrExtensions.cs
@@ -0,0 +1,111 @@
+﻿namespace Bny.General.Memory;
+
+/// <summary>
+/// Extension methods for ConstPtr
+/// </summary>
+public static class ConstPtrExtensions
+{
+    /// <summary>
+    /// Finds the index of the value in the fiven span that starts on multiple
+    /// of the value length
+    /// </summary>
+    /// <example>
+    /// if the ptr would be 'abbcdd' and the value 'dd', the function
+    /// would return 4, but if the value would be 'bb' the function would
+    /// return -1 because 'bb' doesn't occur on index of multiple of the
+    /// value.Length (2)
+    /// </example>
+    /// <typeparam name="T">Type of data in the pointers</typeparam>
+    /// <param name="ptr">The pointer where to search</param>
+    /// <param name="value">The value to find</param>
+    /// <returns>
+    /// Index of the first occurence of value on an index of multiple of
+    /// value.Length, if there is no such index, returns -1
+    /// </returns>
+    public static int IndexOfRepeat<T>(
+        this ConstPtr<T> ptr  ,
+             ConstPtr<T> value)
+    {
+        for (int i = 0; ptr.Length - i > value.Length; i += value.Length)
+        {
+            if (ptr[i..].StartsWith(value))
+                return i;
+        }
+        return -1;
+    }
+
+    /// <summary>
+    /// Finds the index of the first occurence of the given pointer
+    /// using the default equality comparer
+    /// </summary>
+    /// <typeparam name="T">Type of data in the pointer</typeparam>
+    /// <param name="ptr">Pointer in which to search</param>
+    /// <param name="value">Value to find</param>
+    /// <returns>
+    /// Index of the first occurrence, -1 if there is no occurence
+    /// </returns>
+    public static int IndexOf<T>(this ConstPtr<T> ptr, ConstPtr<T> value)
+    {
+        for (int i = 0; ptr.Length >= value.Length; ++i, ++ptr)
+        {
+            if (ptr.StartsWith(value))
+                return i;
+        }
+        return -1;
+    }
+
+    /// <summary>
+    /// Checks whether the pointer starts with the value in the other pointer
+    /// using the default equality comparer
+    /// </summary>
+    /// <typeparam name="T">Type of values in the pointer</typeparam>
+    /// <param name="ptr">Pointer which start will be checked</param>
+    /// <param name="value">Pointer that should start</param>
+    /// <returns>True if ptr starts with value, otherwise false</returns>
+    public static bool StartsWith<T>(this ConstPtr<T> ptr, ConstPtr<T> value)
+    {
+        if (ptr.Length < value.Length)
+            return false;
+
+        for (; value; ++ptr, ++value)
+        {
+            if (+ptr is null)
+            {
+                if (+value is not null)
+                    return false;
+                continue;
+            }
+
+            if (!ptr.Value!.Equals(+value))
+                return false;
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// Finds the index of the first occurence of the given value in the
+    /// pointer using the default equality comparer
+    /// </summary>
+    /// <typeparam name="T">Type of value to find</typeparam>
+    /// <param name="ptr">Pointer to memory where to search</param>
+    /// <param name="value">Value to find</param>
+    /// <returns>Index of the first occurence of value in ptr</returns>
+    public static int IndexOf<T>(this ConstPtr<T> ptr, T value)
+    {
+        for (int i = 0; ptr; ++ptr, ++i)
+        {
+            if (value is null)
+            {
+                if (+ptr is null)
+                    return i;
+                continue;
+            }
+
+            if (value.Equals(+ptr))
+                return i;
+        }
+
+        return -1;
+    }
+}
